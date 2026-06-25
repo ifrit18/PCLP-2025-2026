ec879b468c9272112eacf2da7b109b5d4ac1f3e7
ifrit18 <niculescudaria18@gmail.com>
2026-06-25 16:51:26 +0300

    Finished 2nd problem and resolved coding style mistakes

--- helicopters.c
+++ helicopters.c
@@ -1,6 +1,134 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_VAL 1005
+int a[MAX_VAL][MAX_VAL];
+
+typedef struct coordonate {
+	int r1, c1, r2, c2, s, s1, s2;
+} coordonate;
+
+void citire_teren(int n, int m)
+{
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
+			if (scanf("%d", &a[i][j]) != 1)
+				return;
+		}
+	}
+}
+
+void proceseaza_elicopter(coordonate c_curr, int *corecte, int *is_wrong)
+{
+	// Aflu marginile din ipoteză
+	int r_min, c_min;
+	if (c_curr.r1 < c_curr.r2)
+		r_min = c_curr.r1;
+	else
+		r_min = c_curr.r2;
+
+	if (c_curr.c1 < c_curr.c2)
+		c_min = c_curr.c1;
+	else
+		c_min = c_curr.c2;
+
+	int l_cateta = abs(c_curr.r1 - c_curr.r2) + 1;
+
+	// gasesc dimensiunea varfului
+	if (c_curr.s == 1) {
+		if (c_curr.r1 < c_curr.r2)
+			c_curr.s1 = c_curr.r1;
+		else
+			c_curr.s1 = c_curr.r2;
+
+		if (c_curr.r1 > c_curr.r2)
+			c_curr.s2 = c_curr.c1;
+		else
+			c_curr.s2 = c_curr.c2;
+	} else {
+		if (c_curr.r1 > c_curr.r2)
+			c_curr.s1 = c_curr.r1;
+		else
+			c_curr.s1 = c_curr.r2;
+
+		if (c_curr.r1 < c_curr.r2)
+			c_curr.s2 = c_curr.c1;
+		else
+			c_curr.s2 = c_curr.c2;
+	}
+
+	int total_triunghi = 0;
+	int umbra_triunghi = 0;
+
+	// parcurg patratul de la r_min si c_min
+	for (int i = r_min; i < r_min + l_cateta; i++) {
+		for (int j = c_min; j < c_min + l_cateta; j++) {
+			int inside = 0;
+			int dist_r = abs(i - c_curr.s1);
+			int dist_c = abs(j - c_curr.s2);
+			if (dist_r + dist_c < l_cateta)
+				inside = 1;
+
+			if (inside) {
+				total_triunghi++;
+				if (a[i][j] == 0)
+					umbra_triunghi++;
+			}
+		}
+	}
+
+	if (umbra_triunghi == 0)
+		(*corecte)++;
+
+	if (umbra_triunghi > total_triunghi / 2)
+		*is_wrong = 1;
+}
 
 int main(void)
 {
-return 0;
+	int n, m, k;
+	int corecte_total = 0, gresite = 0;
+	int vesct_gresite[MAX_VAL];
+	coordonate coord[MAX_VAL];
+
+	if (scanf("%d %d", &n, &m) != 2)
+		return 0;
+
+	citire_teren(n, m);
+
+	if (scanf("%d", &k) != 1)
+		return 0;
+
+	for (int idx = 1; idx <= k; idx++) {
+		scanf("%d %d %d %d %d", &coord[idx].r1, &coord[idx].c1,
+			  &coord[idx].r2, &coord[idx].c2, &coord[idx].s);
+
+		// validare pozitii
+		if (coord[idx].c1 == coord[idx].c2 ||
+			coord[idx].r1 == coord[idx].r2 ||
+			abs(coord[idx].r1 - coord[idx].r2) !=
+			abs(coord[idx].c1 - coord[idx].c2)) {
+			printf("Elicopterul %d este pozitionat ", idx);
+			printf("necorespunzator!\n");
+			continue;
+		}
+
+		int is_wrong = 0;
+		proceseaza_elicopter(coord[idx], &corecte_total, &is_wrong);
+
+		if (is_wrong) {
+			vesct_gresite[gresite] = idx;
+			gresite++;
+		}
+	}
+
+	// afisare rezultate
+	printf("%d\n", corecte_total);
+	printf("%d\n", gresite);
+	for (int i = 0; i < gresite; i++) {
+		printf("%d ", vesct_gresite[i]);
+	}
+	printf("\n");
+
+	return 0;
 }
--- window_join.c
+++ window_join.c
@@ -2,56 +2,54 @@
 
 #define MAX 10001
 
-typedef struct vect {
-long long t;
-long long x;
+typedef struct {
+	long long t;
+	long long x;
 } vect;
 
-// function that calculates the cmmdc of two numbers
 long long cmmdc(long long a, long long b)
 {
-if (b == 0)
-return a;
-else
-return cmmdc(b, a % b);
+	if (b == 0)
+		return a;
+	return cmmdc(b, a % b);
 }
 
-// function that calculates the cmmdc of two numbers
 long long cmmmc(long long a, long long b)
 {
-  long long d = cmmdc(a, b);
-  return (a * b) / d;
+	long long d = cmmdc(a, b);
+	return (a / d) * b;
 }
 
 int main(void)
 {
-short window;
-scanf("%hd", &window);
+	short window;
+	vect v[MAX];
+	short count = 0;
 
-vect v[MAX];
-short count = 0;
+	if (scanf("%hd", &window) != 1)
+		return 0;
 
-while (count < MAX) {
-    long long current_t, current_x;
-    scanf("%lld %lld", &current_t, &current_x);
+	while (count < MAX) {
+		long long current_t, current_x;
+		if (scanf("%lld %lld", &current_t, &current_x) != 2)
+			break;
+		if (current_t == 0 && current_x == 0)
+			break;
 
-    if (current_t == 0 && current_x == 0) break;
+		v[count].x = current_x;
+		v[count].t = current_t;
+		count++;
+	}
 
-    v[count].x = current_x;
-    v[count].t = current_t;
-    count++;
-    
-  }
+	for (int i = 0; i < count; i++) {
+		for (int j = i + 1; j < count; j++) {
+			if (v[j].t - v[i].t <= (long long)window) {
+				long long v_cmmdc = cmmdc(v[i].x, v[j].x);
+				long long v_cmmmc = cmmmc(v[i].x, v[j].x);
 
-  for (int i = 0; i < count; i++) {
-    for (int j = i + 1; j < count; j++) {
-      if (v[j].t - v[i].t <= (long long)window) {
-        long long v_cmmdc = cmmdc(v[i].x, v[j].x);
-        long long v_cmmmc = cmmmc(v[i].x, v[j].x);
-        
-
-        printf("%lld %lld\n", v_cmmmc, v_cmmdc);
-      }
-    }
-  }
+				printf("%lld %lld\n", v_cmmmc, v_cmmdc);
+			}
+		}
+	}
+	return 0;
 }
